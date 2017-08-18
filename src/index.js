c4bf33dcdbf20f0edf6660c578c151b56c85b15a
danigb <danigb@gmail.com>
2017-08-18 22:34:39 +0200

    @call and @defn

--- src/index.js
+++ src/index.js
@@ -10,10 +10,9 @@ let nextId = 1;
  * @return {Process} 
  */
 export function process(program = [], time = 0, parent = null) {
-  const proc = (lib, maxTime = Infinity, maxOps = 1000) => {
-    const ops = proc.operations;
-    while (proc.time < maxTime && maxOps && ops.length) {
-      const op = ops.pop();
+  const proc = (lib = {}, maxTime = Infinity, maxOps = 1000) => {
+    while (proc.time < maxTime && --maxOps && proc.operations.length) {
+      const op = proc.operations.pop();
       if (typeof op === "function") op(proc, lib);
       else proc.stack.push(op);
     }
@@ -31,7 +30,7 @@ export function process(program = [], time = 0, parent = null) {
 }
 
 // PRIVATE: invoke a function from a library
-function invoke (name, proc, lib) {
+function invoke (proc, name, lib) {
   const fn = lib[name];
   if (typeof fn === 'function') {
     fn(proc, lib);
@@ -41,12 +40,41 @@ function invoke (name, proc, lib) {
 }
 
 export const lib = {
+  /**
+   * @wait
+   * 
+   * @example
+   * [0.5, '@wait']
+   */
   wait: (proc) => {
     const delay = proc.stack.pop();
     proc.time += delay;
   },
+
+  /**
+   * @call: invoke the function with the name specified in the stack
+   * 
+   * @example
+   * ['pluck', '@call']
+   */
   call: (proc, lib) => {
-    invoke(proc.stack.pop(), proc, lib);
+    const name = proc.stack.pop();
+    invoke(proc, name, lib);
+  },
+
+  /**
+   * @defn: define a function
+   * 
+   * @example
+   * [['@wait', '@pluck'], 'dnote', 0.5, '@dnote']
+   */
+  defn: (proc, lib) => {
+    const name = proc.stack.pop();
+    const program = proc.stack.pop();
+    lib[name] = (proc) => {
+      let i = program.length
+      while (i--) proc.operations.push(program[i]);
+    }
   }
 }
 
@@ -72,10 +100,12 @@ export function scheduler (baseLib = {}, clock = jsclock()) {
   let time = 0;
   const queue = [];
 
+  // add scheduling functions to library
   const lib = Object.assign(baseLib, {
 
   });
 
+  // insert a process in a priority queue
   function schedule (proc) {
     let i = queue.length - 1;
     while (i >= 0 && queue[i].time < proc.time) {
@@ -85,6 +115,7 @@ export function scheduler (baseLib = {}, clock = jsclock()) {
     return proc
   }
 
+  // resume process for a duration (expressed in seconds)
   function resume (duration, limit = 1000) {
     const endsAt = time + duration;
     let proc = queue.pop();
@@ -97,7 +128,10 @@ export function scheduler (baseLib = {}, clock = jsclock()) {
     time = endsAt;
   }
 
-  const run = (program) => schedule(process(program, time));
+  // API: return a function to run programs
+  function run (program, delay) {
+    return schedule(process(program, time));
+  }
   run.stop = clock(resume);
   run.lib = lib
   return run;
--- tests/process.spec.js
+++ tests/process.spec.js
@@ -4,7 +4,7 @@ jest.useFakeTimers();
 
 const logger = () => {
   const log = (proc) => {
-    log.output.push(proc.stack.pop());
+    log.output.push(`${proc.stack.pop()}:${proc.time}`);
   }
   log.output = [];
   return log;
@@ -13,8 +13,8 @@ const logger = () => {
 describe('process', () => {
   it('execs program', () => {
     const log = logger();
-    process([1, log, 2, log])();
-    expect(log.output).toEqual([1, 2]);
+    process(['A', log, 'B', log])();
+    expect(log.output).toEqual(['A:0', 'B:0']);
   })
 })
 
@@ -22,20 +22,23 @@ describe('scheduler', () => {
   it('runs a program in time', () => {
     const log = logger();
     const run = scheduler({}, jsclock(0.3));
-    run([0.5, lib.wait, 'hi', log]);
+    run([0.5, lib.wait, 'A', log]);
     jest.runTimersToTime(100);
     expect(log.output).toEqual([]);
     jest.runTimersToTime(1000);
-    expect(log.output).toEqual(['hi']);
+    expect(log.output).toEqual(['A:0.5']);
   })
-  it('coroutine procs', () => {
+
+  it('coroutine style scheduling', () => {
     const log = logger();
     const run = scheduler();
     run([0.1, lib.wait, 'A1', log, 0.8, lib.wait, 'A2', log]);
-    run([0.2, lib.wait, 'B1', log, 0.3, lib.wait, 'B2', log]);
-    run([0.3, lib.wait, 'C1', log, 0.3, lib.wait, 'C2', log]);
+    run([0.2, lib.wait, 'B1', log, 0.5, lib.wait, 'B2', log]);
+    run([0.25, lib.wait, 'C1', log, 0.25, lib.wait, 'C2', log]);
     jest.runTimersToTime(1000);
-    expect(log.output).toEqual(['A1', 'B1', 'C1', 'B2', 'C2', 'A2']);
+    expect(log.output).toEqual([
+      'A1:0.1', 'B1:0.2', 'C1:0.25', 
+      'C2:0.5', 'B2:0.7', 'A2:0.9']);
   })
 })
 
@@ -48,6 +51,12 @@ describe('core library', () => {
   test('call', () => {
     const log = logger();
     process(['hi', 'log', lib.call])({ log });
-    expect(log.output).toEqual(['hi']);
+    expect(log.output).toEqual(['hi:0']);
+  })
+
+  test('defn', () => {
+    const log = logger();
+    process([['hello', log, log], 'hi', lib.defn, 'one', 'hi', lib.call])();
+    expect(log.output).toEqual(['hello:0', 'one:0']);
   })
 })
